516201b2ed73b55abf2efda2c171bd94889fadee
thiagoribeiro2003 <thiagoribeiro20003@gmail.com>
2022-10-20 09:34:31 -0300

    CSS atualizado da página Produtos

--- src/pages/Produtos/Produtos.jsx
+++ src/pages/Produtos/Produtos.jsx
@@ -3,6 +3,37 @@ const Produtos = () => {
   return (
     <section>
       <h2 className={estilos.titulo_secao}>Conheça nossos produtos</h2>
+      <div className={estilos.produtos}>
+        <article>
+          <h3>Produto 1</h3>
+          <p>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ab enim
+            ducimus dicta saepe sed possimus aperiam, iusto provident labore at
+            voluptatem laborum. Harum nihil autem dolore repellendus, quibusdam
+            eaque consequatur.
+          </p>
+        </article>
+
+        <article>
+          <h3>Produto 2</h3>
+          <p>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ab enim
+            ducimus dicta saepe sed possimus aperiam, iusto provident labore at
+            voluptatem laborum. Harum nihil autem dolore repellendus, quibusdam
+            eaque consequatur.
+          </p>
+        </article>
+
+        <article>
+          <h3>Produto 3</h3>
+          <p>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ab enim
+            ducimus dicta saepe sed possimus aperiam, iusto provident labore at
+            voluptatem laborum. Harum nihil autem dolore repellendus, quibusdam
+            eaque consequatur.
+          </p>
+        </article>
+      </div>
     </section>
   );
 };

6b977096d76df2f68cabdd78edacf32c75cd77b1
thiagoribeiro2003 <thiagoribeiro20003@gmail.com>
2022-10-20 09:52:47 -0300

    Finalizando um subcomponente ListaPosts

--- src/components/ListaPosts/ListaPosts.jsx
+++ src/components/ListaPosts/ListaPosts.jsx
@@ -1,5 +1,48 @@
+import estilos from "./ListaPosts.module.css";
 const ListaPosts = () => {
-  return <div>ListaPosts</div>;
+  return (
+    <div className={estilos.lista_posts}>
+      <article className={estilos.post}>
+        <h3>Título do post...</h3>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus,
+          libero sed enim eum numquam, quis veniam error, consequatur obcaecati
+          fugiat eos provident sequi natus qui distinctio quidem quos
+          dignissimos aliquid?
+        </p>
+      </article>
+
+      <article className={estilos.post}>
+        <h3>Título do post...</h3>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus,
+          libero sed enim eum numquam, quis veniam error, consequatur obcaecati
+          fugiat eos provident sequi natus qui distinctio quidem quos
+          dignissimos aliquid?
+        </p>
+      </article>
+
+      <article className={estilos.post}>
+        <h3>Título do post...</h3>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus,
+          libero sed enim eum numquam, quis veniam error, consequatur obcaecati
+          fugiat eos provident sequi natus qui distinctio quidem quos
+          dignissimos aliquid?
+        </p>
+      </article>
+
+      <article className={estilos.post}>
+        <h3>Título do post...</h3>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus,
+          libero sed enim eum numquam, quis veniam error, consequatur obcaecati
+          fugiat eos provident sequi natus qui distinctio quidem quos
+          dignissimos aliquid?
+        </p>
+      </article>
+    </div>
+  );
 };
 
 export default ListaPosts;

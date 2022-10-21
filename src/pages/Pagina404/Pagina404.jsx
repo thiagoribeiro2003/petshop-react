8eaa23a35ab70ad527ccdfb470dc4ad6f7e93c9b
thiagoribeiro2003 <thiagoribeiro20003@gmail.com>
2022-10-21 09:05:22 -0300

    404

--- src/pages/Pagina404/Pagina404.jsx
+++ src/pages/Pagina404/Pagina404.jsx
@@ -1,7 +1,21 @@
 import estilos from "./Pagina404.module.css";
+import imagem404 from "../../assets/images/404.svg";
 
 const Pagina404 = () => {
-  return <div>Pagina404</div>;
+  return (
+    <section className={estilos.pagina404}>
+      <h2 className={estilos.titulo_secao}>Au, au, au, foi mal!</h2>
+
+      <div id="pagina404">
+        <h3>Ops! Página não encontrada!</h3>
+        <img
+          src={imagem404}
+          className={estilos.imagem404}
+          alt="Ilustração cachorrinho bunitinho"
+        />
+      </div>
+    </section>
+  );
 };
 
 export default Pagina404;

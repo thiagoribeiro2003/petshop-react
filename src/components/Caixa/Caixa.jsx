aba4f0928c47795902fb2092bd24f8ab510a24d1
thiagoribeiro2003 <thiagoribeiro20003@gmail.com>
2022-10-21 10:07:26 -0300

    Se não tiver listaDeClasses, deixamos string vazia, para nao aparecer undefined

--- src/components/Caixa/Caixa.jsx
+++ src/components/Caixa/Caixa.jsx
@@ -1,7 +1,11 @@
 import estilos from "./Caixa.module.css";
 
 const Caixa = (props) => {
-  return <div className={estilos.caixa}>{props.children}</div>;
+  return (
+    <div className={`${estilos.caixa} ${props.listaDeClasses || ""}`}>
+      {props.children}
+    </div>
+  );
 };
 
 export default Caixa;

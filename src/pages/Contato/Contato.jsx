de1b67defff777b97ddf5212d75f1471e8fb7a92
thiagoribeiro2003 <thiagoribeiro20003@gmail.com>
2022-10-28 09:30:45 -0300

    Estilizando formulário contato

--- src/pages/Contato/Contato.jsx
+++ src/pages/Contato/Contato.jsx
@@ -1,3 +1,4 @@
+import { Button, TextField } from "@mui/material";
 import Caixa from "../../components/Caixa/Caixa";
 import estilos from "./Contato.module.css";
 
@@ -7,9 +8,48 @@ const Contato = () => {
       <h2 className={estilos.titulo_secao}>Fale Conosco</h2>
 
       <Caixa>
-        <p>
-          <i>Em breve teremos um formulário com biblioteca de componentes...</i>
-        </p>
+        <form method="post">
+          <div>
+            <TextField
+              type="text"
+              label="Nome"
+              variante="outlined"
+              fullWidth
+              required
+              helperText="Campo obrigatório"
+            />
+          </div>
+
+          <div>
+            <TextField
+              type="email"
+              label="E-mail"
+              variante="outlined"
+              fullWidth
+              required
+              helperText="Insira um e-mail para contato"
+            />
+          </div>
+
+          <div>
+            <TextField
+              type="text"
+              label="Mensagem"
+              variante="outlined"
+              fullWidth
+              required
+              helperText="Fale o que você quiser"
+              multiline
+              rows={6}
+            />
+          </div>
+
+          <div>
+            <Button type="submit" variant="contained">
+              Enviar Mensagem
+            </Button>
+          </div>
+        </form>
       </Caixa>
     </section>
   );

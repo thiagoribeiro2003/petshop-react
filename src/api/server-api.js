1e483b33ffc3f59a85f014e282bf5d21dd5ab304
thiagoribeiro2003 <thiagoribeiro20003@gmail.com>
2022-11-01 09:27:54 -0300

    Configurações para testes via rede

--- src/api/server-api.js
+++ src/api/server-api.js
@@ -1,3 +1,5 @@
 const porta = "2112";
-const serverApi = `http://localhost:${porta}`;
+
+// const serverApi = `http://localhost:${porta}`; // VERSÃO LOCALHOST
+const serverApi = `http://10.20.45.20:${porta}`; // VERSÃO REDE LOCAL VIA IP
 export default serverApi;
